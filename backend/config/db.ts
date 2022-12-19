584a622ebbd0de8e1cf955fc9c80b3ddbfcdc192
Kubix007 <kuba.dolinski@hotmail.com>
2022-12-19 19:21:08 +0100

    :D

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -3,10 +3,7 @@ import "colorts/lib/string";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      process.env.MONGO_URI ??
-        "mongodb+srv://root:root123@hearthstonecardscluster.wxwbgvy.mongodb.net/HearthstoneCardsDB?retryWrites=true&w=majority"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI!);
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(error);
