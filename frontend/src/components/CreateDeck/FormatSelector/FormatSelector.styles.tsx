9eb82485d161c3be355f5c83c576cf9c1a3c7bd6
Kubix007 <kuba.dolinski@hotmail.com>
2023-02-09 08:38:06 +0100

    Format Selector Fix

--- frontend/src/components/CreateDeck/FormatSelector/FormatSelector.styles.tsx
+++ frontend/src/components/CreateDeck/FormatSelector/FormatSelector.styles.tsx
@@ -3,6 +3,7 @@ import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button/Button";
 import { ReactComponent as StandardFormatImage } from "./../../../img/CreateDeck/StandardFormat.svg";
 import { Typography } from "@mui/material";
+import * as Types from "./FormatSelector.types";
 
 export const FormatSelectorContainer = styled(Grid)``;
 
@@ -29,20 +30,32 @@ export const FormatName = styled(Typography)`
   }
 `;
 
-export const StandardFormatSVG = styled(StandardFormatImage)`
+export const StandardFormatSVG = styled(StandardFormatImage)<Types.SVGProps>`
   width: 70%;
   fill: rgb(134, 105, 60);
+  z-index: 2;
 `;
 
 export const StandardFormatButton = styled(Button)`
   &.MuiButton-root {
-    width: 78px;
+    z-index: 0;
+    width: 73px;
     height: 85px;
-    transition: all 0.15s ease-out 0s;
-    background-image: url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt43b47368ca583e7d/6037d7d8c6713d4e7a4d4eb5/deckbuilder_tab_standard.png);
+    background: url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt63ce92e33c79ee7f/622902fe04503350d255bca6/YotH_SVG-01-01.svg)
+        no-repeat center/70%,
+      url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt43b47368ca583e7d/6037d7d8c6713d4e7a4d4eb5/deckbuilder_tab_standard.png);
     &:hover {
       background: url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt851e1166ca161eb3/6036baec415d5f4e803250b1/deckbuilder_tab_hover.webp)
           center,
+        url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt63ce92e33c79ee7f/622902fe04503350d255bca6/YotH_SVG-01-01.svg)
+          no-repeat center/70%,
+        url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt43b47368ca583e7d/6037d7d8c6713d4e7a4d4eb5/deckbuilder_tab_standard.png);
+    }
+    &.selected-format {
+      background: url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt851e1166ca161eb3/6036baec415d5f4e803250b1/deckbuilder_tab_hover.webp)
+          center,
+        url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt63ce92e33c79ee7f/622902fe04503350d255bca6/YotH_SVG-01-01.svg)
+          no-repeat center/70%,
         url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt43b47368ca583e7d/6037d7d8c6713d4e7a4d4eb5/deckbuilder_tab_standard.png);
     }
   }
@@ -59,6 +72,11 @@ export const ClassicFormatButton = styled(Button)`
           center,
         url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/bltcaa946e4f9e0b273/6036baec0b1d853be8ad56ea/deckbuilder_tab_classic.webp);
     }
+    &.selected-format {
+      background: url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt851e1166ca161eb3/6036baec415d5f4e803250b1/deckbuilder_tab_hover.webp)
+          center,
+        url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/bltcaa946e4f9e0b273/6036baec0b1d853be8ad56ea/deckbuilder_tab_classic.webp);
+    }
   }
 `;
 
@@ -69,6 +87,11 @@ export const WildFormatButton = styled(Button)`
     transition: all 0.15s ease-out 0s;
     background-image: url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blta03fd5ddc70cc2d9/6036baec02ccfb4d0f67236d/deckbuilder_tab_wild.webp);
     &:hover {
+      background: url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt851e1166ca161eb3/6036baec415d5f4e803250b1/deckbuilder_tab_hover.webp)
+          center,
+        url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blta03fd5ddc70cc2d9/6036baec02ccfb4d0f67236d/deckbuilder_tab_wild.webp);
+    }
+    &.selected-format {
       background: url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blt851e1166ca161eb3/6036baec415d5f4e803250b1/deckbuilder_tab_hover.webp)
           center,
         url(https://images.blz-contentstack.com/v3/assets/bltc965041283bac56c/blta03fd5ddc70cc2d9/6036baec02ccfb4d0f67236d/deckbuilder_tab_wild.webp);
--- frontend/src/components/CreateDeck/FormatSelector/FormatSelector.tsx
+++ frontend/src/components/CreateDeck/FormatSelector/FormatSelector.tsx
@@ -1,6 +1,32 @@
 import * as Styles from "./FormatSelector.styles";
 
 const FormatSelector = () => {
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    //let manaCost = (event.target as HTMLButtonElement).value;
+    let button = document.getElementById(
+      (event.target as HTMLButtonElement).id
+    );
+    console.log(button);
+    if (button?.getAttribute("id") === "standard-format-svg") {
+      button = document.getElementById("standard-format");
+    }
+    if (button?.getAttribute("class")?.includes("selected-format")) {
+      button?.setAttribute(
+        "class",
+        `${button?.getAttribute("class")!}`
+          .replace("selected-format", "")
+          .trimEnd()
+      );
+      //dispatch(reduceManaCost(manaCost));
+    } else {
+      button?.setAttribute(
+        "class",
+        `${button?.getAttribute("class")!} selected-format`
+      );
+      //dispatch(addManaCost(manaCost));
+    }
+  };
+
   return (
     <Styles.FormatSelectorContainer
       container
@@ -9,17 +35,18 @@ const FormatSelector = () => {
       direction="row"
     >
       <Styles.GridItem item>
-        <Styles.StandardFormatButton>
-          <Styles.StandardFormatSVG />
-        </Styles.StandardFormatButton>
+        <Styles.StandardFormatButton
+          onClick={handleClick}
+          id="standard-format"
+        />
         <Styles.FormatName>Standard</Styles.FormatName>
       </Styles.GridItem>
       <Styles.GridItem item>
-        <Styles.ClassicFormatButton />
+        <Styles.ClassicFormatButton onClick={handleClick} id="classic-format" />
         <Styles.FormatName>Klasyczne</Styles.FormatName>
       </Styles.GridItem>
       <Styles.GridItem item>
-        <Styles.WildFormatButton />
+        <Styles.WildFormatButton onClick={handleClick} id="wild-format" />
         <Styles.FormatName>Dzicz</Styles.FormatName>
       </Styles.GridItem>
     </Styles.FormatSelectorContainer>
